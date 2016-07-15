4f9529a307b33f4485e09ca7338067b86c2b068a
Rob Pendray <robert.pendray@gmail.com>
2016-07-15 22:52:15 +0000

    further improvements

--- rps/rps.rb
+++ rps/rps.rb
@@ -1,11 +1,9 @@
 class Player
   attr_accessor :move, :name
-  
+
   def initialize
     set_name
   end
-  
- 
 end
 
 class Human < Player
@@ -19,24 +17,76 @@ class Human < Player
     end
     self.name = n
   end
-  
+
   def choose
     puts "Please choose rock, paper or scissors:"
     choice = nil
     loop do
       choice = gets.chomp
-      break if ['rock','paper','scissors'].include?(choice)
+      break if Move::VALUES.include?(choice)
       puts "Sorry, invalid choice."
     end
+    self.move = Move.new(choice)
   end
 end
 
 class Computer < Player
   def set_name
-    self.name = ['R2D2','Hal','Chappie','Sonny','Number 5'].sample
+    self.name = ['R2D2', 'Hal', 'Chappie', 'Sonny', 'Number 5'].sample
   end
+
   def choose
-    self.move = ['rock','paper','scissors'].sample
+    self.move = Move.new(Move::VALUES.sample)
+  end
+end
+
+class Move
+  VALUES = ['rock', 'paper', 'scissors'].freeze
+
+  def initialize(value)
+    @value = value
+  end
+
+  def scissors?
+    @value == "scissors"
+  end
+
+  def rock?
+    @value == "rock"
+  end
+
+  def paper?
+    @value == "paper"
+  end
+
+  def >(other_move)
+    if rock?
+      return true if other_move.scissors?
+      return false
+    elsif paper?
+      return true if other_move.rock?
+      return false
+    elsif scissors?
+      return true if other_move.paper?
+      return true
+    end
+  end
+
+  def <(other_move)
+    if rock?
+      return true if other_move.paper?
+      return false
+    elsif paper?
+      return true if other_move.scissors?
+      return false
+    elsif scissors?
+      return true if other_move.rock?
+      return true
+    end
+  end
+
+  def to_s
+    @value.to_s
   end
 end
 
@@ -47,32 +97,25 @@ class RPSGame
     @human = Human.new
     @computer = Computer.new
   end
-  
+
   def display_welcome_message
     puts "Welcome to Rock, Paper, Scissors!"
   end
-  
+
   def display_goodbye_message
     puts "Thanks for playing Rock, Paper, Scissors! Goodbye."
   end
-  
+
   def display_winner
     puts "#{human.name} chose #{human.move}"
     puts "#{computer.name} chose #{computer.move}"
-    
-    case human.move
-    when 'rock'
-      puts "It's a tie!" if computer.move == "rock"
-      puts "#{human.name} won!" if computer.move == "scissors"
-      puts "#{computer.name} won!" if computer.move == "paper"
-    when 'paper'
-      puts "It's a tie!" if computer.move == "paper"
-      puts "#{human.name} " if computer.move == "rock"
-      puts "#{computer.name}" if computer.move == "scissors"
-    when 'scissors'
-      puts "It's a tie!" if computer.move == "scissors  "
-      puts "#{human.name} " if computer.move == "paper"
-      puts "#{computer.name}" if computer.move == "rock"
+
+    if human.move > computer.move
+      puts "#{human.name} won"
+    elsif computer.move > human.move
+      puts "#{computer.name} won"
+    else
+      puts "It's a tie"
     end
   end
 
@@ -81,13 +124,13 @@ class RPSGame
     loop do
       puts "Would you like to play again (y/n)"
       answer = gets.chomp
-      break if ['y','n'].include? answer.downcase
+      break if ['y', 'n'].include? answer.downcase
       puts "Sorry must be y or n"
     end
-    
+
     answer == 'y'
   end
-  
+
   def play
     display_welcome_message
     loop do
